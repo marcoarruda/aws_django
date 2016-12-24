7eabe6ec905745be358a32c727d27502f2eb4619
Marco Arruda <marco.nc.arruda@gmail.com>
2016-12-23 17:56:00 -0800

    subscriptions ok with mysql

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -72,11 +72,11 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'mysql': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangodb',
         'USER': 'djangouser',
